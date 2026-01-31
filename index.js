07e82ab91cc22af6e0bb5872a98506f4c74c2734
uh1k <scpskm@outlook.jp>
2026-01-31 17:57:18 +0900

    Use trimmed hook environment variable in fetch

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import express from 'express';
 
 express()
     .get("/random", async (req, res) => {
+        const hook = process.env.hook?.trim();
         const got = await fetch ("https://ipinfo.io/json").then(r => r.json());
-        await fetch (process.env.hook, {
+        await fetch (hook, {
             method:"POST",
             headers:{"Content-Type":"application/json"},
             body:JSON.stringify({content:`${got.ip} ${got.region} <@1238820524256268381>`})
